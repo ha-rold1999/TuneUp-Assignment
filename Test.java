5c9574c3b2018d276a0befa111cc1b7ad388221e
Harold Cuico <64396407+ha-rold1999@users.noreply.github.com>
2020-09-13 23:14:35 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Test
+{
+   public static void main(String[] args)
+   {
+      Scanner con=new Scanner(System.in);
+      float a;
+      a=con.nextFloat();
+      int b=(int)a;
+      System.out.print(b);
+   }
+}
